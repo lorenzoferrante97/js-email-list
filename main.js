f3d6d07d52007f6bb7ae553d6a88ec3cd92270f2
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-28 14:15:02 +0100

    add: fetch api

--- main.js
+++ main.js
@@ -2,3 +2,23 @@
 
 let elencoMail = document.getElementById("elencoMail");
 let generaMail = document.getElementById("generaMail");
+
+//richiedo 10 volte tramite api la generazione di una mail
+
+for (let i = 0; i < 10; i++) {
+    fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(response => response.json())
+    .then(data => {
+        let email = data.response;
+        elencoMail.innerHTML +=
+        `
+        <!-- email box -->
+        <div class="col">
+            <p>${email}</p>
+        </div>
+        `
+    })
+    .catch(error => {
+    
+    })  
+}
